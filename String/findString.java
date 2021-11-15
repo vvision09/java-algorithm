d0b469f55af27ddd5ed02dddad981f1e96c6b41b
vvision09 <vvision009@gmail.com>
2021-11-15 20:44:01 +0900

    first commit

---
+++ String/findString.java
@@ -0,0 +1,32 @@
+package String;
+
+import java.util.Scanner;
+
+public class findString {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		char c =  sc.next().charAt(0);
+		System.out.println(solution(str,c));
+	}
+	
+	public static int solution(String str, char t) {
+		int answer = 0;
+		str = str.toUpperCase();
+		t = Character.toUpperCase(t);
+	
+		for(char x : str.toCharArray())
+			if(x == t) answer++;
+		
+		/*
+		for(int i = 0; i < str.length(); i++) 
+			if(str.charAt(i) == t) answer++; 
+			*/
+		
+		
+		
+		return answer;
+	}
+	
+}
