6e7f87b03e021e6ca5c542d7b8429394397aa8bf
vvision09 <vvision009@gmail.com>
2021-11-15 20:59:35 +0900

    대소문자 변환

---
+++ String/Convert.java
@@ -0,0 +1,22 @@
+package String;
+
+import java.util.Scanner;
+
+public class Convert {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(solution(str));
+	}
+	public static String solution(String str) {
+		String answer = "";
+		
+		for(char x : str.toCharArray()) {
+			if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
+			else answer += Character.toLowerCase(x);
+		}
+		return answer;
+	}
+	
+}
